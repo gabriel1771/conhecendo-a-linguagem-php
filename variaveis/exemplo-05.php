807d68961b3fc8cc35388f49127936bec25a1170
gabriel-dev <gabrielrenhardtdosreis@gmail.com>
2021-12-14 23:30:47 -0300

    escopo de variaveis com a propiedade Global na frente da variavel

---
+++ variaveis/exemplo-05.php
@@ -0,0 +1,18 @@
+<?php
+
+$nome = "Glaucio";
+
+function teste() {
+    global $nome;
+    echo $nome;
+}
+
+function teste2() {
+    $nome = "joão";
+    echo $nome ."agora no teste 2";
+}
+
+teste();
+
+teste2();
+?>
